020522e0d26f1d20f173a2ef7e9bbf58c7e55b39
benjamin rosenheim <benjaminrosenheim1@gmail.com>
2020-09-24 21:14:58 -0500

    bot is fully implemented: input string, output LaTeX formatted image.

--- bot.py
+++ bot.py
@@ -8,11 +8,10 @@ from discord.ext import commands
 c = commands.Bot(command_prefix='~')
 
 k = os.environ.get('TEXTOKEN')
-i = '$$\int_0^1 e^x\,dx$$'
 def convert(i):
     sp.preview(i, viewer='file', filename='o.png', euler=False)
     src = cv2.imread('o.png')
-    _, alpha = cv2.threshold(cv2.cvtColor(~src, cv2.COLOR_BGR2GRAY), 63, 255, cv2.THRESH_BINARY)
+    _, alpha = cv2.threshold(cv2.cvtColor(~src, cv2.COLOR_BGR2GRAY), 64, 255, cv2.THRESH_BINARY)
     b, g, r = cv2.split(~src)
     rgba = [b, g, r, alpha]
     dst = cv2.merge(rgba, 4)
@@ -21,6 +20,13 @@ def convert(i):
 @c.event
 async def on_ready():
     print(1)
-    convert(i)
+
+@c.event
+async def on_message(message):
+    if message.content.startswith('~'):
+        print(f'received \"{message.content}\"\n')
+        convert(message.content[1:len(message.content):1])
+        channel = message.channel
+        await channel.send(file=discord.File('o.png'))
 
 c.run(k)
